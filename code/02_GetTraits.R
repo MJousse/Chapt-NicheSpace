eba752c52c9de5ba1a25e362da539724795b7ee9
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-02-23 12:14:41 -0500

    Continue trait cleaning

--- code/02_GetTraits.R
+++ code/02_GetTraits.R
@@ -1,10 +1,13 @@
 rm(list = ls())
 
 library(dplyr)
+library(rgbif)
+library(purrr)
+source("code/functions.R")
 
 # Add functional traits for the species in each FW ------------------------
 
-### Get global trait database 
+### Get global trait database - Pretty long
 
 # Etard et al. 2020 (https://doi.org/10.1111/geb.13184)
 Amphi_traits <- read.csv("data/raw/traits/traits_etard2020/Amphibians.csv")
@@ -23,24 +26,96 @@ write.csv(Tetrapod_traits, "data/checkpoints/TetrapodTraits.csv")
 ### European metaweb
 Euro_species <- read.csv("data/cleaned/EuroMWTaxo.csv", row.names = 1)
 Euro_traits <- Tetrapod_traits %>%
-  select(-Order, - Family, -Genus) %>%
-  right_join(Euro_species, by = c("Best_guess_binomial" = "Species"))
+  select(-Order, -Family, -Genus, -Best_guess_binomial) %>%
+  right_join(Euro_species, by = "Species") %>%
+  filter(!is.na(Species))
+
+# Take the mode of non-numeric columns
+Euro_traits_nonnum <- Euro_traits %>%
+  select_at(vars(Species, Trophic_level:Other.Unknown, Class:Genus)) %>%
+  group_by(Species) %>%
+  summarise_all(getmode) %>%
+  mutate_at(vars(-Species), as.factor)
+
+# Take the mean of numeric columns
+Euro_traits_num <- Euro_traits %>%
+  select_at(vars(Species, Body_length_mm:Litter_clutch_size, Adult_svl_cm:Generation_length_d)) %>%
+  group_by(Species) %>%
+  summarise_all(mean, na.rm = T) %>%
+  mutate_at(vars(-Species), as.numeric)
+
+Euro_traits <- full_join(Euro_traits_nonnum, Euro_traits_num)
 
 ### Serengeti FW
 Serengeti_species <- read.csv("data/cleaned/SerengetiFWTaxo.csv", row.names = 1)
 Serengeti_traits <- Tetrapod_traits %>%
-  select(-Order, - Family, -Genus) %>%
-  right_join(Serengeti_species, by = c("Best_guess_binomial" = "Species"))
+  select(-Order, -Family, -Genus, -Best_guess_binomial) %>%
+  right_join(Serengeti_species, by = "Species") %>%
+  filter(!is.na(Species))
+
+# Take the mode of non-numeric columns
+Serengeti_traits_nonnum <- Serengeti_traits %>%
+  select_at(vars(Species, Trophic_level:Other.Unknown, Class:Genus)) %>%
+  group_by(Species) %>%
+  summarise_all(getmode) %>%
+  mutate_at(vars(-Species), as.factor)
+
+# Take the mean of numeric columns
+Serengeti_traits_num <- Serengeti_traits %>%
+  select_at(vars(Species, Body_length_mm:Litter_clutch_size, Adult_svl_cm:Generation_length_d)) %>%
+  group_by(Species) %>%
+  summarise_all(mean, na.rm = T) %>%
+  mutate_at(vars(-Species), as.numeric)
+
+Serengeti_traits <- full_join(Serengeti_traits_nonnum, Serengeti_traits_num)
 
 ### Pyrennees FW
 Pyrennees_species <- read.csv("data/cleaned/pyrenneesFWTaxo.csv", row.names = 1)
 Pyrennees_traits <- Tetrapod_traits %>%
-  select(-Order, - Family, -Genus) %>%
-  right_join(Pyrennees_species, by = c("Best_guess_binomial" = "Species"))
+  select(-Order, -Family, -Genus, -Best_guess_binomial) %>%
+  right_join(Pyrennees_species, by = "Species") %>%
+  filter(!is.na(Species))
+
+# Take the mode of non-numeric columns
+Pyrennees_traits_nonnum <- Pyrennees_traits %>%
+  select_at(vars(Species, Trophic_level:Other.Unknown, Class:Genus)) %>%
+  group_by(Species) %>%
+  summarise_all(getmode) %>%
+  mutate_at(vars(-Species), as.factor)
+
+# Take the mean of numeric columns
+Pyrennees_traits_num <- Pyrennees_traits %>%
+  select_at(vars(Species, Body_length_mm:Litter_clutch_size, Adult_svl_cm:Generation_length_d)) %>%
+  group_by(Species) %>%
+  summarise_all(mean, na.rm = T) %>%
+  mutate_at(vars(-Species), as.numeric)
+
+Pyrennees_traits <- full_join(Pyrennees_traits_nonnum, Pyrennees_traits_num)
 
 ### High Arctic FW
 HighArctic_species <- read.csv("data/cleaned/HighArcticFWTaxo.csv", row.names = 1)
 HighArctic_traits <- Tetrapod_traits %>%
-  select(-Order, - Family, -Genus) %>%
-  right_join(HighArctic_species, by = c("Best_guess_binomial" = "Species"))
+  select(-Order, -Family, -Genus, -Best_guess_binomial) %>%
+  right_join(HighArctic_species, by = "Species") %>%
+  filter(!is.na(Species))
 
+# Take the mode of non-numeric columns
+HighArctic_traits_nonnum <- HighArctic_traits %>%
+  select_at(vars(Species, Trophic_level:Other.Unknown, Class:Genus)) %>%
+  group_by(Species) %>%
+  summarise_all(getmode) %>%
+  mutate_at(vars(-Species), as.factor)
+
+# Take the mean of numeric columns
+HighArctic_traits_num <- HighArctic_traits %>%
+  select_at(vars(Species, Body_length_mm:Litter_clutch_size, Adult_svl_cm:Generation_length_d)) %>%
+  group_by(Species) %>%
+  summarise_all(mean, na.rm = T) %>%
+  mutate_at(vars(-Species), as.numeric)
+
+HighArctic_traits <- full_join(HighArctic_traits_nonnum, HighArctic_traits_num)
+
+write.csv(Euro_traits, "data/cleaned/EuroTraits.csv")
+write.csv(Serengeti_traits, "data/cleaned/SerengetiTraits.csv")
+write.csv(Pyrennees_traits, "data/cleaned/PyrenneesTraits.csv")
+write.csv(HighArctic_traits, "data/cleaned/HighArcticTraits.csv")
