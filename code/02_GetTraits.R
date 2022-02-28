8f3ec1cf0303836c49ff814e20a723986447b3d5
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-02-28 11:41:38 -0500

    Impute missing traits

--- code/02_GetTraits.R
+++ code/02_GetTraits.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(dplyr)
 library(rgbif)
 library(purrr)
+library(missForest)
 source("code/functions.R")
 
 # Add functional traits for the species in each FW ------------------------
@@ -115,7 +116,65 @@ HighArctic_traits_num <- HighArctic_traits %>%
 
 HighArctic_traits <- full_join(HighArctic_traits_nonnum, HighArctic_traits_num)
 
-write.csv(Euro_traits, "data/cleaned/EuroTraits.csv")
-write.csv(Serengeti_traits, "data/cleaned/SerengetiTraits.csv")
-write.csv(Pyrennees_traits, "data/cleaned/PyrenneesTraits.csv")
-write.csv(HighArctic_traits, "data/cleaned/HighArcticTraits.csv")
+# Checkpoint
+write.csv(Euro_traits, "data/checkpoints/EuroTraits.csv")
+write.csv(Serengeti_traits, "data/checkpoints/SerengetiTraits.csv")
+write.csv(Pyrennees_traits, "data/checkpoints/PyrenneesTraits.csv")
+write.csv(HighArctic_traits, "data/checkpoints/HighArcticTraits.csv")
+
+Euro_traits <- read.csv("data/checkpoints/EuroTraits.csv", row.names = 1)
+Serengeti_traits <- read.csv("data/checkpoints/SerengetiTraits.csv", row.names = 1)
+Pyrennees_traits <- read.csv("data/checkpoints/PyrenneesTraits.csv", row.names = 1)
+HighArctic_traits <- read.csv("data/checkpoints/HighArcticTraits.csv", row.names = 1)
+
+
+### Impute missing data for each class separated
+Tetrapods_traits <- bind_rows(Euro_traits, Serengeti_traits, Pyrennees_traits, HighArctic_traits) %>%
+  distinct() %>%
+  mutate_at(vars(Trophic_level:Diel_activity, Artificial_habitat_use:Genus), as.factor)
+
+# amphibians
+Amphi_traits <- filter(Tetrapods_traits, Class == "Amphibia") %>%
+  droplevels()
+sp <- Amphi_traits$Species
+traits <- Amphi_traits %>% select(-Species)
+
+Amphi_traits_full <- missForest(traits)
+Amphi_traits_full <- data.frame(Species=sp, Amphi_traits_full$ximp) %>%
+  left_join(select(Amphi_traits, Species, Genus, Family))
+
+# birds
+Bird_traits <- filter(Tetrapods_traits, Class == "Aves") %>%
+  droplevels()
+sp <- Bird_traits$Species
+traits <- Bird_traits %>% select(-Species, -Genus, -Family,- Habitat_breadth_IUCN)
+
+Bird_traits_full <- missForest(traits)
+Bird_traits_full <- data.frame(Species=sp, Bird_traits_full$ximp) %>%
+  left_join(select(Bird_traits, Species, Genus, Family))
+
+# mammals
+Mam_traits <- filter(Tetrapods_traits, Class == "Mammalia") %>%
+  droplevels()
+sp <- Mam_traits$Species
+traits <- Mam_traits %>% select(-Species, -Genus)
+
+Mam_traits_full <- missForest(traits)
+Mam_traits_full <- data.frame(Species=sp, Mam_traits_full$ximp) %>%
+  left_join(select(Mam_traits, Species, Genus, Family))
+
+# reptiles
+Rept_traits <- filter(Tetrapods_traits, Class == "Reptilia") %>%
+  droplevels()
+sp <- Rept_traits$Species
+traits <- Rept_traits %>% select(-Species, -Genus)
+
+Rept_traits_full <- missForest(traits)
+Rept_traits_full <- data.frame(Species=sp, Rept_traits_full$ximp) %>%
+  left_join(select(Rept_traits, Species, Genus, Family))
+
+# put everything together
+Tetrapods_traits <- bind_rows(Amphi_traits_full, Bird_traits_full, Mam_traits_full, Rept_traits_full) %>%
+  select(-Adult_svl_cm, -Longevity_d, -Generation_length_d, -Body_length_mm)
+
+write.csv(Tetrapods_traits, "data/cleaned/SpeciesTraitsFull.csv")
