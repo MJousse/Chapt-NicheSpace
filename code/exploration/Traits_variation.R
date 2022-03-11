de3c7aac247698ac21f373e3d5f86eb1b0f1f10b
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-03-11 10:01:45 -0500

    trait variation

--- code/exploration/Traits_variation.R
+++ code/exploration/Traits_variation.R
@@ -1,13 +1,32 @@
 rm(list = ls())
 library(lme4)
-library(MuMIn)
+library(nnet)
+library(dplyr)
+library(performance)
 
-EuroTraits <- read.csv("data/cleaned/SpeciesTraitsFull.csv",, row.names = 1, stringsAsFactors = T)
+EuroTraits <- read.csv("data/cleaned/SpeciesTraitsFull.csv", row.names = 1, stringsAsFactors = T) %>%
+  mutate(TrophicLevel = as.difelse(Herbivore == 1, "Herbivore", 
+                               ifelse(Carnivore == 1, "Carnivore", 
+                                      ifelse(Omnivore ==1, "Omnivore", NA))), .keep = "unused")
 
-# Habitat_breadth
-habitat_breadth<- lmer(Habitat_breadth_IUCN ~ (1 | Class/Order/Family/Genus), data = EuroTraits, REML = F)
+traits_col <- c(2:15, 20:23)
 
-AIC.table  <- model.sel(habitat_breadth_0, habitat_breadth_1, habitat_breadth_2, habitat_breadth_3, habitat_breadth_4)
-
-# Diel_activity
-diel_activity <- glmer(Diel_activity ~ (1 | Class/Order/Family/Genus), data = EuroTraits, family = "binomial")
+df <- data.frame()
+for (itrait in traits_col){
+  trait <- colnames(EuroTraits)[itrait]
+  formula <- as.formula(paste0(trait, " ~ (1 | Class/Order/Family/Genus)"))
+  if (length(unique(EuroTraits[,itrait])) == 2){
+    model <- brm(formula, 
+                 data = EuroTraits, family = bernoulli())
+    var_partition <- icc(model, by_group = T)
+  } else if (trait == "TrophicLevel"){
+    model <- brm(formula, data = EuroTraits, family = categorical())
+    var_partition <- icc(model, by_group = T)
+  } else {
+    model <- brm(formula, data = EuroTraits, family = gaussian())
+    var_partition <- icc(model, by_group = T)
+  }
+  df <- rbind(df,
+              c(trait, var_partion$ICC, (1 - sum(ICC))))
+}
+colnames(df) <- c("Trait", "Genus", "Family", "Order", "Class", "Residuals")
