90a9b02432608b7980a9ae20f7d2879a9691a839
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-03-21 16:36:26 -0400

    comment 03 and 04

--- code/03_Model.R
+++ code/03_Model.R
@@ -1,35 +1,36 @@
+# Step 03: Train the predictive model on the European metaweb
+# 1. Prepare the training dataset
+# 2. Set up the GLMM with greta package
+# 3. Train the model
+
 rm(list = ls())
 set.seed(16)
 library(dplyr)
 library(greta)
 source("code/functions.R")
 
-# Load data and standardize -----------------------------------------------
+# Prepare training dataset ------------------------------------------------
+# load data and standardize
 EuroInteractions <- read.csv("data/cleaned/EuroFWadults.csv", row.names = 1)
 FuncTraits <- read.csv("data/cleaned/SpeciesTraitsFull.csv", row.names = 1)
 EuroSpecies <- read.csv("data/cleaned/EuroMWTaxo.csv", row.names = 1) %>%
   distinct() %>%
   filter(!is.na(Species))
 
-# Transform trait into predictors for every species pair ------------------
-EuroMW <- expand.grid(EuroSpecies$Species, EuroSpecies$Species) 
-colnames(EuroMW) <- c("Predator", "Prey")
+# transform trait into predictors for every species pair
+EuroMW <- get_predictors(EuroSpecies$Species, FuncTraits)
 
-EuroMW <- left_join(EuroMW, FuncTraits, by = c("Prey" = "Species")) %>%
-  left_join(FuncTraits, by = c("Predator" = "Species"))
-
-EuroMW <- traits2predictors(EuroMW)
+# scale predictors
 EuroMW <- mutate_at(EuroMW, vars(Habitat_breadth.predator, BM.predator:ClutchSize.predator,
                                  Habitat_breadth.prey, BM.prey:ClutchSize.prey, 
                                  Habitat.match:BM.match), scale2)
 
+# add response
 EuroInteractions$interaction <- 1
-
 EuroMW <- left_join(EuroMW, EuroInteractions)
 EuroMW$interaction[is.na(EuroMW$interaction)] <- 0
 
-# Divide into training and testing data -----------------------------------
-# Let's take 70% of positives and as many negatives for training
+# take 70% of positives and as many negatives for training
 N = round(0.7 * sum(EuroMW$interaction))
 training_positives <- sample(seq_len(sum(EuroMW$interaction)), size = N)
 training_negatives <- sample(seq_len(sum(EuroMW$interaction==0)), size = N)
@@ -39,41 +40,47 @@ training_id <- as.numeric(c(
 ))
 training <- EuroMW[training_id, ]
 
-# GLMM --------------------------------------------------------------------
+# Set up the bayesian glmm ------------------------------------------------
+# extract predictors
 predictors <- select(training, 
                      -Predator, -Prey, -Order.predator, -Order.prey,
                      -Herbivore.predator, -Herbivore.prey, -interaction,
                      -ClutchSize.prey, -ClutchSize.predator)
 
+# add a column for the intercept
 predictors <- cbind(rep(1, nrow(training)), predictors) %>% as_data()
 y <- as_data(training$interaction)
+
+# get levels for the random effect: predator order
 predator_order <- as.numeric(factor(training$Order.pred, levels = unique(FuncTraits$Order)))
 
+# number of predictors and orders
 norder <- length(unique(FuncTraits$Order))
 ntraits <- ncol(predictors)
 
 # common effect
 global_coef_mean <- normal(0,1, ntraits)
 global_coef_sd <- cauchy(0, 5, truncation = c(0.001, Inf), dim = ntraits)
+
+# coef for each order is drawn from global mean and sd
 coef <- normal(global_coef_mean, global_coef_sd, dim = c(ntraits))
 for (i in c(2:norder)){
   coef <- cbind(coef, normal(global_coef_mean, global_coef_sd, dim = c(ntraits)))
 }
 
-# parameters of the base model are a function of the order of the predator
-linear_predictor <- rowSums(predictors * t(coef[,predator_order]))
+# linear prediction
+linear_predicton <- rowSums(predictors * t(coef[,predator_order]))
 
-# ilogit of linear predictor
-p <- ilogit(linear_predictor)
+# ilogit of linear prediction
+p <- ilogit(linear_predicton)
 
-# a single observation means our data are bernoulli distributed
+# interaction as Bernoulli trial
 distribution(y) <- bernoulli(p)
 
+# Train the model with greta ----------------------------------------------
 m <- model(global_coef_mean, global_coef_sd)
-
 GLMM <- mcmc(m, n_samples = 15000, warmup = 10000, chains = 4)
 
+# save the model
 save(GLMM, global_coef_mean, global_coef_sd, training_id, training, coef,  
      file =  paste0("data/models/GLMM_", format(Sys.Date(), "%d%m%Y"), ".RData"))
-
-
