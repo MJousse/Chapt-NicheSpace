47b154ef58eada8d69de023d7318f2b74ffd3f3f
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-07-07 12:09:07 -0400

    local changes

--- code/11_FWproperties.R
+++ code/11_FWproperties.R
@@ -4,7 +4,7 @@
 # 2. Calculate the trophic roles of all species in predicted webs
 # For each webs, use 100 posterior samples and use the mean.
 # 3. Calulate the errors for each role metrics of all species
-
+set.seed(16)
 library(igraph)
 library(dplyr)
 library(tidyr)
@@ -47,13 +47,13 @@ empirical_properties <- bind_rows(
 )
 write.csv(empirical_properties, file = "data/checkpoints/EmpiricalProperties.csv")
 
-# Trophic roles in predicted webs -----------------------------------------
+# FW properties in predicted webs -----------------------------------------
 library(foreach)
 library(doParallel)
 load("data/checkpoints/predictions.RData")
 foodwebs <- c("Arctic", "Euro", "Pyrenees", "Serengeti")
 combinations <- expand_grid(Source = foodwebs, Target = foodwebs)
-predicted_roles <-c()
+predicted_properties <-c()
 # for each combination of source and target webs, use 100 posterior sample
 # calculate the roles of all species in these sample, and extract the mean
 for (combination in c(1:nrow(combinations))){
@@ -64,20 +64,21 @@ for (combination in c(1:nrow(combinations))){
   predictions <- get(paste0(sourceFW, "_", targetFW, "_predictions"))
   cl <- makeCluster(4) 
   registerDoParallel(cl)
-  role <- foreach(i=c(1:100), .combine = rbind, 
+  properties <- foreach(i=c(1:100), .combine = rbind, 
                   .packages = c("igraph", "NetIndices", "dplyr", "tidyr")) %dopar% {
                     prediction <- data.frame(resource = predictions$Prey, 
                                              consumer = predictions$Predator, 
                                              interaction = predictions[,paste0("draws",i)])
                     prediction <- prediction[prediction$interaction == 1,]
-                    fw_properties(prediction, ncores = 0)
+                    fw_properties(prediction, nsim = 10)
                   }
   stopCluster(cl)
-  fw_properties_mean <- apply(fw_properties, MARGIN = 1, mean)
-  fw_properties_sd <- apply(fw_properties, MARGIN = 1, sd)
-  predicted_roles <- rbind(predicted_roles, 
-                           pivot_longer(role_mean, -species, names_to = "role", values_to = "predicted") %>% mutate(targetFW = targetFW, sourceFW = sourceFW))
-  write.csv(predicted_roles, file = "data/checkpoints/predicted_roles.csv")
+  properties_mean <- apply(properties, MARGIN = 1, mean)
+  properties_sd <- apply(fw_properties, MARGIN = 1, sd)
+  predicted_properties <- rbind(predicted_properties, 
+                                data.frame(t(properties_mean)) %>% pivot_longer(everything(), names_to = "metric", values_to = "predicted") %>%
+                                  mutate(targetFW = targetFW, sourceFW = sourceFW))
+  write.csv(predicted_properties, file = "data/checkpoints/PredictedProperties.csv")
 }
 
 # Compare the empirical roles and predicted roles -------------------------
