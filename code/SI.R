2f16c4e99e0f8c57993a71c152a37dbf3f844709
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-07-18 13:54:38 -0400

    other metrics plot

---
+++ code/SI.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# Appendix other performance metrics --------------------------------------
+overall_performance <- read.csv("data/checkpoints/overall_performance_draws.csv", row.names = 1) %>%
+  pivot_longer(cols = tpr:npv, names_to = "metric")
+
+ggplot(overall_performance) +
+  geom_density(aes(value, after_stat(scaled) ,fill = Source), alpha = .5) +
+  facet_grid(Target~metric, scales = "free")
+
+# how correlated is auc to aucpr and other metrics
+library(corrplot)
+overall_performance_othermetrics <- read.csv("data/checkpoints/overall_performance_draws.csv", row.names = 1) %>%
+  group_by(Source, Target) %>%
+  summarise_all(median)
+
+overall_performance_auc <- read.csv("data/checkpoints/overall_performance.csv", row.names=1)
+
+overall_performance <- full_join(overall_performance_othermetrics, overall_performance_auc)
+
+Movr <- cor(overall_performance[,c("auc", "aucpr", "tpr", "tnr", "ppv", "npv")])
+corrplot(Movr, method = "number", type = "upper", diag = F)
+
+species_performance_othermetrics <- read.csv("data/checkpoints/species_performance_draws.csv", row.names = 1) %>%
+  group_by(Source, Target, species) %>%
+  summarise_all(median, na.rm = T)
+
+species_performance_auc <- read.csv("data/checkpoints/species_performance.csv", row.names=1)
+
+species_performance <- full_join(species_performance_othermetrics, species_performance_auc)
+
+Msp <- cor(species_performance[,c("auc", "aucpr", "tpr", "tnr", "ppv", "npv")])
+corrplot(Msp, method = "number", type = "upper", diag = F)
