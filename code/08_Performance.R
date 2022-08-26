5b9f5b6bd0af49aec4bd5e2c2ac69436aeeb7d54
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2022-08-26 11:25:40 -0400

    update performance model and plot

--- code/08_Performance.R
+++ code/08_Performance.R
@@ -29,12 +29,15 @@ for (combination in c(1:nrow(overall_performance))){
   sourceFW <- overall_performance[combination, "Source"]
   targetFW <- overall_performance[combination, "Target"]
   predictions <- get(paste0(sourceFW, "_", targetFW, "_predictions"))
-  overall_performance[combination, "auc"] <- performance(prediction(predictions$Estimate, predictions$interaction), "auc")@y.values[[1]]
-  overall_performance[combination, "aucpr"] <- performance(prediction(predictions$Estimate, predictions$interaction), "aucpr")@y.values[[1]]
-  overall_performance[combination, "prevalence"] <- sum(predictions$interaction)/nrow(predictions)
+  if (sourceFW == targetFW){
+    predictions_test <- filter(predictions, training == 0)
+  }
+  overall_performance[combination, "auc"] <- performance(prediction(predictions_test$Estimate, predictions_test$interaction), "auc")@y.values[[1]]
+  overall_performance[combination, "aucpr"] <- performance(prediction(predictions_test$Estimate, predictions_test$interaction), "aucpr")@y.values[[1]]
+  overall_performance[combination, "prevalence"] <- sum(predictions_test$interaction)/nrow(predictions_test)
   overall_performance_draws <- rbind(overall_performance_draws,
                                      data.frame(sourceFW = sourceFW, targetFW= targetFW,
-                                           performance_draws(select(predictions, starts_with("draws")), predictions$interaction)))
+                                           performance_draws(select(predictions_test, starts_with("draws")), predictions_test$interaction)))
   fw_sp <- unique(predictions$Predator)
   for (species in fw_sp){
    sp_predictions <- filter(predictions, Predator == species | Prey == species)
@@ -108,26 +111,59 @@ ggsave("figures/ModelTransferability.png", p)
 # glm with overall logit-auc and log(aucpr/prevalence) as response
 # distances as fixed effect
 # source and target region as crossed random effect
-overall_performance <- filter(overall_performance, Source != Target)
 
 # transform responses
 overall_performance$logitauc <- log(overall_performance$auc / (1-overall_performance$auc))
 overall_performance$logaucpr <- log(overall_performance$aucpr / overall_performance$prevalence)
 
 # scale predictors
-overall_performance$geo.dist <- as.vector(scale(overall_performance$geo.dist))
-overall_performance$env.dist <- as.vector(scale(overall_performance$env.dist))
-overall_performance$phylo.dist <- as.vector(scale(overall_performance$phylo.dist))
+overall_performance$geo.dist_sc <- as.vector(scale(overall_performance$geo.dist))
+overall_performance$env.dist_sc <- as.vector(scale(overall_performance$env.dist))
+overall_performance$phylo.dist_sc <- as.vector(scale(overall_performance$phylo.dist))
 
-# model with logit auc as response using brms
-auc_model <- bf(logitauc ~ env.dist + phylo.dist + (1|Source) + (1|Target))
-env_model <- bf(env.dist ~ geo.dist)
-phylo_model <- bf(phylo.dist ~ geo.dist)
+# model with logit auc as response and only geographic distance using brms (total effect of geographic distances)
+auc_geo_total <- brm(logitauc ~ geo.dist_sc + (1|Source) + (1|Target),
+                 data = overall_performance,
+                 prior = c(
+                   prior(normal(0, 1), class = "Intercept"),
+                   prior(normal(0, 1), class = "b"),
+                   prior(cauchy(0, 5), class = "sd")
+                 ), 
+                 sample_prior = "no",
+                 iter = 2000)
 
-auc_sem_model <- brm(auc_model + env_model + phylo_model + set_rescor(FALSE),
-    data = overall_performance, 
-    sample_prior = "no",
-    iter = 2000)
+# model with logit auc as response and geographic distance, controlling for environmental and phylogenetic distances (direct effect of geographic distance)
+auc_geo_direct <- brm(logitauc ~ geo.dist_sc + phylo.dist_sc + env.dist_sc + (1|Source) + (1|Target),
+                     data = overall_performance,
+                     prior = c(
+                       prior(normal(0, 1), class = "Intercept"),
+                       prior(normal(0, 1), class = "b"),
+                       prior(cauchy(0, 5), class = "sd")
+                     ), 
+                     sample_prior = "no",
+                     iter = 2000)
+
+# model with logit auc as response and phylogenetic distance, controlling for geographic distances (effect of phylogenetic distance)
+auc_phylo <- brm(logitauc ~ phylo.dist_sc + geo.dist_sc + (1|Source) + (1|Target),
+               data = overall_performance,
+               prior = c(
+                 prior(normal(0, 1), class = "Intercept"),
+                 prior(normal(0, 1), class = "b"),
+                 prior(cauchy(0, 5), class = "sd")
+               ), 
+               sample_prior = "no",
+               iter = 2000)
+
+# model with logit auc as response and environmental distance, controlling for geographic distances (effect of environmental distance)
+auc_env <- brm(logitauc ~ env.dist_sc + geo.dist_sc + (1|Source) + (1|Target),
+                 data = overall_performance,
+                 prior = c(
+                   prior(normal(0, 1), class = "Intercept"),
+                   prior(normal(0, 1), class = "b"),
+                   prior(cauchy(0, 5), class = "sd")
+                 ), 
+                 sample_prior = "no",
+                 iter = 2000)
 
 # model with log aucpr/prevalence as response using brms
 aucpr_model <- bf(logaucpr ~ env.dist + phylo.dist + (1|Source) + (1|Target))
@@ -138,3 +174,81 @@ aucpr_model <- brm(aucpr_model + env_model + phylo_model + set_rescor(FALSE),
                  data = overall_performance, 
                  sample_prior = "no",
                  iter = 2000)
+
+
+# plot geographic distance
+geo_fe <- tibble(geo.dist_sc = seq(min(overall_performance$geo.dist_sc), 
+                                max(overall_performance$geo.dist_sc), length.out=100), Source = NA, Target = NA) %>%
+  add_epred_draws(auc_geo_total,
+                   re_formula = NA, ndraws = 1e3) %>%
+  mutate(x = (geo.dist_sc * sd(overall_performance$geo.dist) + mean(overall_performance$geo.dist)),
+         y = exp(.epred)/(1+exp(.epred)))
+
+geo_fe <- geo_fe %>% 
+  group_by(x) %>%
+  summarize(mean = mean(y),
+            u95 = quantile(y, 0.975),
+            l95 = quantile(y, 0.025))
+
+p1 <- ggplot(geo_fe,
+       aes(x = x/1000, y = mean)) +
+  geom_lineribbon(aes(ymin = l95, ymax = u95), alpha= 0.5) +
+  geom_point(aes(x = geo.dist/1000, y = auc), data = overall_performance) +
+  labs(y = "AUC", x = "Geographic distance (10³km)")+
+  theme_minimal() +
+  lims(y = c(0.1,1))+
+  theme(axis.line = element_line(size = 0.5), strip.text = element_text(size = 12),
+        strip.background = element_rect(colour = "black"), panel.grid = element_blank())
+
+# plot environmental distance
+env_fe <- tibble(env.dist_sc = seq(min(overall_performance$env.dist_sc), 
+                                   max(overall_performance$env.dist_sc), length.out=100), geo.dist_sc = 0, Source = NA, Target = NA) %>%
+  add_epred_draws(auc_env,
+                  re_formula = NA, ndraws = 1e3) %>%
+  mutate(x = (env.dist_sc * sd(overall_performance$env.dist) + mean(overall_performance$env.dist)),
+         y = exp(.epred)/(1+exp(.epred)))
+
+env_fe <- env_fe %>% 
+  group_by(x) %>%
+  summarize(mean = mean(y),
+            u95 = quantile(y, 0.975),
+            l95 = quantile(y, 0.025))
+
+p2 <- ggplot(env_fe,
+             aes(x = x, y = mean)) +
+  geom_lineribbon(aes(ymin = l95, ymax = u95), alpha= 0.5) +
+  geom_point(aes(x = env.dist, y = auc), data = overall_performance) +
+  labs(y = "AUC", x = "Environmental distance")+
+  lims(y = c(0.1,1))+
+  theme_minimal() +
+  theme(axis.line = element_line(size = 0.5), panel.grid = element_blank(),
+        axis.title.y = element_blank(), axis.text.y = element_blank())
+
+# plot phylogenetic distance
+phylo_fe <- tibble(phylo.dist_sc = seq(min(overall_performance$phylo.dist_sc), 
+                                   max(overall_performance$phylo.dist_sc), length.out=100), geo.dist_sc = 0, Source = NA, Target = NA) %>%
+  add_epred_draws(auc_phylo,
+                  re_formula = NA, ndraws = 1e3) %>%
+  mutate(x = (phylo.dist_sc * sd(overall_performance$phylo.dist) + mean(overall_performance$phylo.dist)),
+         y = exp(.epred)/(1+exp(.epred)))
+
+phylo_fe <- phylo_fe %>% 
+  group_by(x) %>%
+  summarize(mean = mean(y),
+            u95 = quantile(y, 0.975),
+            l95 = quantile(y, 0.025))
+
+p3 <- ggplot(phylo_fe,
+             aes(x = x, y = mean)) +
+  geom_lineribbon(aes(ymin = l95, ymax = u95), alpha= 0.5) +
+  geom_point(aes(x = phylo.dist, y = auc), data = overall_performance) +
+  labs(y = "AUC", x = "Phylogenetic distance")+
+  lims(y = c(0.1,1))+
+  theme_minimal() +
+  theme(axis.line = element_line(size = 0.5), panel.grid = element_blank(),
+        axis.title.y = element_blank(), axis.text.y = element_blank())
+
+p1 + p2 + p3
+ggsave("figures/ModelTransferability.png", width = 9)
+
+       
